38e2efa85b998ff91af751f6732d57d33e1a426a
Lornwri <34406133+Lornwri@users.noreply.github.com>
2024-11-17 20:41:29 -0600

    Initial Commit

---
+++ src/components/StudentRow.vue
@@ -0,0 +1,69 @@
+<script setup>
+import { ref, watch } from 'vue';
+
+const props = defineProps({
+  student: Object,
+});
+
+const emit = defineEmits(['arrived-or-left', 'delete-student']);
+
+// Reactive state for presence
+const isStudentPresent = ref(props.student.present);
+
+// Sync `isStudentPresent` with `props.student.present`
+watch(() => props.student.present, (newVal) => {
+  isStudentPresent.value = newVal;
+});
+
+// Notify parent about arrival or departure
+const notifyArrivedOrLeft = () => {
+  emit('arrived-or-left', props.student, isStudentPresent.value);
+};
+
+// Confirm before deleting a student
+const confirmThenDeleteStudent = () => {
+  if (confirm(`Delete ${props.student.name}?`)) {
+    emit('delete-student', props.student);
+  }
+};
+</script>
+
+
+<template>
+  <tr class="align-middle" :class="{ present: student.present, absent: !student.present }">
+    <td>{{ student.name }}</td>
+    <td>{{ student.starID }}</td>
+    <td>
+      <!-- Checkbox for presence -->
+      <label>
+        <input
+            type="checkbox"
+            v-model="isStudentPresent"
+            @change="notifyArrivedOrLeft"
+        />
+        <span v-if="student.present" class="mx-3">Here!</span>
+        <span v-else class="mx-3">Not present</span>
+      </label>
+    </td>
+    <td>
+      <!-- Delete button -->
+      <button class="btn btn-danger" @click="confirmThenDeleteStudent">
+        <i class="bi bi-trash-fill"></i> Delete
+      </button>
+    </td>
+  </tr>
+</template>
+
+
+
+<style scoped>
+.present {
+  color: gray;
+  font-style: italic;
+}
+
+.absent {
+  color: black;
+  font-weight: bold;
+}
+</style>
